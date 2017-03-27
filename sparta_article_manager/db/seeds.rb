0f13277514ebf19759a0d59bbaaf4e1fa2cde89b
Cyril Mupakile <cmupakile@spartaglobal.co>
2017-03-27 16:32:04 +0100

    skeleton gem addition

--- sparta_article_manager/db/seeds.rb
+++ sparta_article_manager/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+picture1 = Picture.create({ image: 'image1.png', article: '1Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...'})
+picture2 = Picture.create({ image: 'image2.png', article: '2Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...'})
+picture3 = Picture.create({ image: 'image3.png', article: '3Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...'})
+picture4 = Picture.create({ image: 'image4.png', article: '4Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...'})
