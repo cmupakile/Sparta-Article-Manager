ecfa9cc390c26fda13432dfaa55db5c64e968eff
Cyril Mupakile <cmupakile@spartaglobal.co>
2017-03-28 23:28:27 +0100

    articles controller set up

--- sparta_article_manager/app/controllers/articles_controller.rb
+++ sparta_article_manager/app/controllers/articles_controller.rb
@@ -1,2 +1,63 @@
 class ArticlesController < ApplicationController
+before_action :find_article, only: [:edit, :update, :show, :delete]
+
+# display all the posts (articles)
+def index
+  @articles = Article.all
+end
+
+# Create an article (create a post)
+def  new
+  @article = Article.new
+end
+
+# Save the article (save the post)
+def create
+  @article = Article.new
+  if @article.save(article_params)
+    flash[:notice] = "Successfully Created Article Post."
+    redirect_to article_path(@article)
+  else
+    flash[:alert] = "Error. Not Created."
+    render :new
+  end
+end
+
+# Edit func? get the post and render the edit page
+def edit
+end
+
+# Update the article with new information
+def Update
+  if @article.update_attributes(article_params)
+    flash[:notice] = "Article Updated."
+    redirect_to article_path(@articles)
+  else
+    flash[:alert] = "Error Updating the Article."
+    render :edit
+  end
+end
+
+# display the individual articles after getting the ID (SHOW)
+def show
+end
+
+# Destroy action. Delete the article permanently from the db
+def Destroy
+  if @article.Destroyflash[:notice] = "Article Deleted."
+    redirect_to article_path
+  else
+    flash[:alert] = "Error Deleting Article."
+  end
+end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
+  def find_article
+    @article = Post.find(params[:id])
+  end
+
 end
