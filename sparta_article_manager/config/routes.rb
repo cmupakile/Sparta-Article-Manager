240f48ce9925c47aa4119e55b7ecb88241bd6ab1
Cyril Mupakile <cmupakile@spartaglobal.co>
2017-03-29 00:13:02 +0100

    CRUD templates created. route configuration + root route

--- sparta_article_manager/config/routes.rb
+++ sparta_article_manager/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  
 
+# set this as the root.
+# if root is selected in the browser. display index page
+root to: "articles#index"
+
+# allows all the CRUD actions
+resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
